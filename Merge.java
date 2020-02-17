71e3b5aa5dbc38bbc43bcba84b62066584b804fc
MarianaM98 <32773783+MarianaM98@users.noreply.github.com>
2020-02-16 20:14:38 -0600

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,20 +1,24 @@
 import java.util.*;
 public class Merge {
-    
+    //Funcion tomada de: Data Structures in Java for the Principled Programmer
+    //Duane A. Bailey
     public void merge(int data[],int temp[], int low, int middle, int high)
     {
         int ri = low;
         int ti= low;
         int di =middle;
+        //while two lists are not empty merge smaller value
         while(ti<middle && di <=high)
         {
             if(data[di]<temp[ti])
             {
                 data[ri++]=data[di++];
+                //smaller is in high data
             }
             else
             {
                 data[ri++]=temp[ti++];
+                //smaller is in temp
             }
         }
         while(ti<middle)
@@ -29,6 +33,7 @@ public class Merge {
         int middle= low+n/2;
         int i;
         if(n<2)
+            //move lower half of data into temporary storage
         {
             return;
         }
@@ -36,8 +41,11 @@ public class Merge {
         {
             temp[i]=data[i];
         }
+        //sort lower half of array
         mergeSortRecursive(temp,data,low,middle-1);
+        //sort upper half of array
         mergeSortRecursive(data,temp,middle,high);
+        //merge halves together
         merge(data,temp,low,middle,high);
     }
     
