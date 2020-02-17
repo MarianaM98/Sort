2de27c036f2b5d12d77b66fcfbc016b489d7ec25
MarianaM98 <32773783+MarianaM98@users.noreply.github.com>
2020-02-16 20:11:03 -0600

    Update Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -1,13 +1,16 @@
 import java.util.*;
 public class Insertion {
-    
+    //Funcion tomada de: Data Structures in Java for the Principled Programmer
+    //Duane A. Bailey
     public void insertionSort(int data[],int n)
     {
         int numSorted=1;
         int index;
         while(numSorted<n)
         {
+            //take the first unsorted value
             int temp =data[numSorted];
+            //and insert it among the sorted
             for(index=numSorted;index>0;index--)
             {
                 if(temp<data[index-1])
@@ -19,6 +22,7 @@ public class Insertion {
                     break;
                 }
             }
+            //reinsert value
             data[index]=temp;
             numSorted++;
         }
